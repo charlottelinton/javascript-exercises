dc5f445ad19f8165c36636db5ccd0a005a25c234
calinton <charlottelinton@users.noreply.github.com>
2019-01-28 21:55:42 -0800

    numbered output only

---
+++ cardDeck.js
@@ -0,0 +1,15 @@
+for (let w = 1; w < 14; w++) {
+  console.log(w + " " + 'of spades');
+}
+
+for (let x = 1; x < 14; x++) {
+  console.log(x + " " + 'of hearts');
+}
+
+for (let y = 1; y < 14; y++) {
+  console.log(y + " " + 'of clubs');
+}
+
+for (let z = 1; z < 14; z++) {
+  console.log(z + " " + 'of diamonds');
+}
