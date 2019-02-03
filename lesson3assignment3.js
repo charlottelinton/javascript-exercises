43528549eb166bd5ffdf8882865c996f455211f7
calinton <charlottelinton@users.noreply.github.com>
2019-02-03 13:43:55 -0800

    console log elements, index, and comma

---
+++ lesson3assignment3.js
@@ -0,0 +1,5 @@
+let arr = ['Gandalf', 'Aragorn', 'Legolas', 'Gimli', 'Boromir', 'Frodo', 'Samwise', 'Pippin', 'Merry'];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(i + ', ' + arr[i]);
+}
